ff46aea7c7d9719291ac09a1068fbfa06d0c46bc
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-08-16 19:20:49 +0300

    timetable example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import bgImage from './assets/bg.jpg';
 import Events from './components/Events';
 import Header from './components/Header';
+import Timetable from './components/Timetable';
 
 export default function App() {
   return (
     <div className="relative">
       <img src={bgImage} className="absolute -z-50" />
       <Header />
-      <main>
+      <main className="p-4">
+        <Timetable />
         <Events />
       </main>
     </div>
---
+++ src/components/Timetable.tsx
@@ -0,0 +1,132 @@
+export default function Timetable() {
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <td className="w-8"></td>
+            <td>PUG</td>
+            <td>1 klasė</td>
+            <td>2 klasė</td>
+            <td>3 klasė</td>
+            <td>4 klasė</td>
+            <td>5 klasė</td>
+            <td>6 klasė</td>
+            <td>7 klasė</td>
+            <td>8 klasė</td>
+            <td>9 klasė</td>
+            <td>10 klasė</td>
+            <td>11 klasė</td>
+            <td>12 klasė</td>
+            <td>12 klasė</td>
+          </tr>
+          <tr>
+            <td>0</td>
+            <td>Etika/ Tikyba</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Matematika</td>
+            <td>Lietuvių kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Anglų kalba</td>
+            <td>Lietuvių kalba</td>
+            <td>Etika/ Tikyba</td>
+            <td>Istorija</td>
+            <td>Fizika</td>
+            <td>Technologijos</td>
+            <td>A Chemija</td>
+            <td>Matematika</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Keramika</td>
+            <td>Lietuvių kalba</td>
+            <td>Matematika</td>
+            <td>Baltarusių kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Istorija</td>
+            <td>Matematika</td>
+            <td>Fizinis ugdymas</td>
+            <td>Geografija</td>
+            <td>Etika/ Tikyba</td>
+            <td>Matematika</td>
+            <td>Fizika Biologija</td>
+            <td>Anglų kalba</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Lietuvių kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Matematika</td>
+            <td>Matematika</td>
+            <td>Matematika</td>
+            <td>Geografija</td>
+            <td>Lietuvių kalba</td>
+            <td>Etika/ Tikyba</td>
+            <td>Baltarusių kalba</td>
+            <td>Fizika</td>
+            <td>Anglų kalba</td>
+            <td>Lietuvių kalba</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Baltarusių kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Fizinis ugdymas</td>
+            <td>Lietuvių kalba</td>
+            <td>Lietuvių kalba</td>
+            <td>Rusų kalba</td>
+            <td>Matematika</td>
+            <td>Baltarusių kalba</td>
+            <td>Šiuolaikiniai menai</td>
+            <td>Etika/ Tikyba</td>
+            <td>Anglų kalba</td>
+            <td>Lietuvių kalba</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Fizinis ugdymas</td>
+            <td>Dailė ir technologijos</td>
+            <td>Lietuvių kalba</td>
+            <td>Pasaulio pažinimas</td>
+            <td>Etika/ Tikyba</td>
+            <td>Baltarusių kalba</td>
+            <td>Fizika</td>
+            <td>Rusų kalba</td>
+            <td>Matematika</td>
+            <td>Šiuolaikiniai menai</td>
+            <td>Lietuvių kalba</td>
+            <td>Fizinis ugdymas</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Dailė ir technologijos</td>
+            <td>Fizinis ugdymas</td>
+            <td>Rusų kalba</td>
+            <td>Dailė</td>
+            <td>Baltarusių kalba</td>
+            <td>Fizika</td>
+            <td>Anglų kalba</td>
+            <td>Fizinis ugdymas</td>
+            <td>Lietuvių kalba</td>
+            <td>Tikyba</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>Fizinis ugdymas</td>
+            <td>Anglų kalba</td>
+            <td>Baltarusių kalba</td>
+            <td>Dailė</td>
+            <td>Lietuvių kalba</td>
+            <td>Lietuvių kalba</td>
+            <td>Anglų kalba mod.</td>
+            <td>Šiuolaikiniai menai (M)</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
