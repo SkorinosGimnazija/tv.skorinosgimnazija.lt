c231b1d160e5f752aa749cd365967455edaa04a9
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-09-08 19:58:21 +0300

    bg test

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import FakeTimetable from './components/FakeTimetable';
 
 export default function App() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <img className="absolute -z-50 object-cover w-full h-full" src={bg} />
+    <div
+      style={{ backgroundImage: `url(${bg})` }}
+      className="relative w-screen h-screen overflow-hidden bg-cover bg-no-repeat bg-center"
+    >
+      {/* <img className="absolute -z-50 object-cover w-full h-full" src={bg} /> */}
       <main className="p-8 grid grid-cols-12 grid-rows-6 w-full h-full">
         <div className="row-span-2 col-span-5">
           <Announcement />
