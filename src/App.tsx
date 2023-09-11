70e3f647500130c083f5592f375792e2116861cc
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-09-11 17:42:01 +0300

    no ev

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Announcement from './components/Announcement';
 import BackgroundImage from './components/BackgroundImage';
-import Events from './components/Events';
+// import Events from './components/Events';
 // import Timetable from './components/Timetable';
 
 export default function App() {
@@ -12,9 +12,7 @@ export default function App() {
           <Announcement />
         </div>
         <div className="col-start-9 row-span-full col-span-4">{/* <Timetable /> */}</div>
-        <div className="row-start-5 row-span-2 col-span-5">
-          <Events />
-        </div>
+        <div className="row-start-5 row-span-2 col-span-5">{/* <Events /> */}</div>
       </main>
     </div>
   );
