90f04ad5b3aabc83469b9775b50fdc70f954675c
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-09-11 10:25:04 +0300

    timetable row

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -2,7 +2,7 @@ export default function Announcement() {
   return (
     <div className="bg-background shadow-lg rounded-xl p-4 relative">
       <span className="absolute bg-orange-500 w-6 h-6 rounded-full top-0 right-0 animate-ping"></span>
-      <p className="text-4xl text-center">
+      <p className="text-5xl text-center">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
         been the industrys standard dummy text ever since the 1500s, when an unknown printer took a
         galley of type
