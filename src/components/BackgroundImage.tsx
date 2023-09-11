c2bf13e501038ccc360184a817755ededf8593ca
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-09-11 17:35:15 +0300

    no img

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useRef } from 'react';
-import useSWR from 'swr';
-import { minuteToMs } from '../utils/lib';
+// import { useEffect, useRef } from 'react';
+// import useSWR from 'swr';
+// import { minuteToMs } from '../utils/lib';
 
 export default function BackgroundImage() {
-  const img1 = useRef<HTMLImageElement>(null);
-  const img2 = useRef<HTMLImageElement>(null);
-  const visibleImg = useRef<HTMLImageElement>();
+  // const img1 = useRef<HTMLImageElement>(null);
+  // const img2 = useRef<HTMLImageElement>(null);
+  // const visibleImg = useRef<HTMLImageElement>();
 
-  const { data, error } = useSWR<AnnouncementDto[]>(`/school/public/random-image`, {
-    refreshInterval: minuteToMs(45),
-  });
+  // const { data, error } = useSWR<AnnouncementDto[]>(`/school/public/random-image`, {
+  //   refreshInterval: minuteToMs(45),
+  // });
 
-  useEffect(() => {
-    if (img1.current == null || img2.current == null || data == null || error) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (img1.current == null || img2.current == null || data == null || error) {
+  //     return;
+  //   }
 
-    if (visibleImg.current === img1.current) {
-      visibleImg.current = img2.current;
-    } else {
-      visibleImg.current = img1.current;
-    }
+  //   if (visibleImg.current === img1.current) {
+  //     visibleImg.current = img2.current;
+  //   } else {
+  //     visibleImg.current = img1.current;
+  //   }
 
-    visibleImg.current.src = import.meta.env.VITE_STATIC_URL + '/' + data;
-  }, [data, error]);
+  //   visibleImg.current.src = import.meta.env.VITE_STATIC_URL + '/' + data;
+  // }, [data, error]);
 
-  const onImageLoad = () => {
-    img1.current!.classList.toggle('opacity-0');
-    img2.current!.classList.toggle('opacity-0');
-  };
+  // const onImageLoad = () => {
+  //   img1.current!.classList.toggle('opacity-0');
+  //   img2.current!.classList.toggle('opacity-0');
+  // };
 
   return (
     <>
-      <img
+      {/* <img
         ref={img1}
         onLoad={onImageLoad}
         className="opacity-0 absolute object-cover w-screen h-screen transition-opacity duration-1000"
@@ -41,7 +41,7 @@ export default function BackgroundImage() {
         ref={img2}
         onLoad={onImageLoad}
         className="absolute object-cover w-screen h-screen transition-opacity duration-1000"
-      />
+      /> */}
     </>
   );
 }
