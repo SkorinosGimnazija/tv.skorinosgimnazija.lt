61b677ee8300b92f6f9ecaff8ec066ef7aa89f4c
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-09-11 21:56:35 +0300

    text ov animation

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -1,30 +1,50 @@
+import { useEffect, useRef } from 'react';
 import useSWR from 'swr';
 import { minuteToMs } from '../utils/lib';
 
 export default function Timetable() {
+  const refs = useRef<(HTMLSpanElement | null)[]>([]);
   const { data } = useSWR<TimetableDto>(`/timetable/public/today`, {
-    refreshInterval: minuteToMs(1),
+    refreshInterval: minuteToMs(10),
   });
+
+  useEffect(() => {
+    for (const element of refs.current) {
+      if (element == null) {
+        continue;
+      }
+
+      if (element.clientWidth < element.scrollWidth) {
+        element.classList.add('animate-text-overflow');
+      }
+    }
+
+    refs.current = [];
+  }, [data]);
 
   if (!data) {
     return null;
   }
 
   return (
-    <section className="text-5xl text-center relative">
-      <p className="bg-background shadow-lg rounded-lg font-extrabold p-2 border-b-4 border-b-yellow-300 mb-2">
+    <section className="text-5xl relative">
+      <p className="bg-background text-center shadow-lg rounded-lg font-extrabold p-2 border-b-4 border-b-yellow-300 mb-2">
         {data.classtime.number} Pamoka {data.classtime.startTime} - {data.classtime.endTime}
       </p>
       {data.timetable.map((x) => {
         return (
-          <p
+          <div
             key={x.id}
-            className="bg-background shadow-lg mb-2 rounded-lg p-[0.15rem] text-[2.75rem] text-ellipsis overflow-hidden whitespace-nowrap"
+            className="bg-background shadow-lg mb-2 rounded-lg p-[0.15rem] text-[2.75rem] whitespace-nowrap flex gap-2"
           >
-            <span className="font-bold">{x.classRoom}</span>
-            {': '}
-            {x.className}
-          </p>
+            <span className="font-bold">
+              {x.classRoom}
+              {':  '}
+            </span>
+            <div className="overflow-hidden">
+              <p ref={(e) => refs.current.push(e)}>{x.className}</p>
+            </div>
+          </div>
         );
       })}
     </section>
